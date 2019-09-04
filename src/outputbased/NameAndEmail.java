6152f5673b98f3123719f2a396747cfaf0e5a498
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2019-09-04 23:00:53 +0530

    variable declared and initialized and ouput displayed

---
+++ src/outputbased/NameAndEmail.java
@@ -0,0 +1,15 @@
+package outputbased;
+
+public class NameAndEmail {
+    public static void main(String[] args) {
+
+
+        String name = "ASHISH";
+        long mobile = 583758357;
+        String email = "ashish@gmail.com";
+        System.out.println(name);
+        System.out.println(mobile);
+        System.out.println(email);
+    }
+
+}
