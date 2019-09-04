62a956621bf4d19a88dd3b6fe7089b92933029af
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2019-09-04 21:49:17 +0530

    variable declared

---
+++ src/DeclareAndInitialize.java
@@ -0,0 +1,9 @@
+public class DeclareAndInitialize {
+    public static void main(String[] args) {
+        int input1=20;
+        int input2=30;
+        System.out.println("input1="+input1);
+        System.out.println("input2="+input2);
+
+    }
+}
