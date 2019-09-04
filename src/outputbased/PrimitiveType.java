036f5c8f94b9358f3ec6860be90b44ce5f4d6f2a
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2019-09-04 22:03:51 +0530

    VARIABLE DECLARED

---
+++ src/outputbased/PrimitiveType.java
@@ -0,0 +1,24 @@
+package outputbased;
+
+public class PrimitiveType {
+    public static void main(String[] args) {
+        int n = 2;
+        float f = 2.9f;
+        char c = 'a';
+        double d = 2.00;
+        boolean b = true;
+        byte a = 126;
+        short s = 56;
+        long l = 8676766273L;
+        System.out.println("int=" + n);
+        System.out.println("float=" + f);
+        System.out.println("char=" + c);
+        System.out.println("double=" + d);
+        System.out.println("boolean=" + b);
+        System.out.println("byte=" + a);
+        System.out.println("short=" + s);
+        System.out.println("long=" + l);
+    }
+
+
+}
