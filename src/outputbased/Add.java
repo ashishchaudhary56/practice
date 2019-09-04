a7cae48eefce48f9ba7dd03915a77582f0e71e45
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2019-09-04 22:42:23 +0530

    ouput

---
+++ src/outputbased/Add.java
@@ -0,0 +1,11 @@
+package outputbased;
+
+public class Add {
+    public static void main(String[] args) {
+        double a = 3.14;
+        double b = 5.0;
+        double d;
+        d = a + b;
+        System.out.println(d);
+    }
+}
