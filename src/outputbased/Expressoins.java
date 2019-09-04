38577d6a53cc90cbc0f87e6ff498033079e627a2
Ashish Chaudhary <ashish.chaudhary_cs18@gla.ac.in>
2019-09-04 22:50:28 +0530

    variable declared and initialized and ouput displayed

---
+++ src/outputbased/Expressoins.java
@@ -0,0 +1,21 @@
+package outputbased;
+
+public class Expressoins {
+    public static void main(String[] args) {
+        double a, b, c, d, e;
+        int u;
+
+        a = -2 % 5;
+        b = -2 % -5;
+        c = 2 % -5;
+        d = 23 / 10;
+        e = 23.0 / 10;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(u = 2 + 3);
+
+    }
+}
